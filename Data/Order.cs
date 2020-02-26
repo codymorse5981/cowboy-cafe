0a2ce93334da3cda7b70d64d792e0245b29b2904
cody598 <cody598@F1092-23-2111>
2020-02-26 14:52:21 -0600

    commit ordertest.cs

---
+++ Data/Order.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+
+namespace CowboyCafe.Data
+{
+    public class Order : INotifyPropertyChanged
+    {
+        private uint lastOrderNumber;
+
+        private List<IOrderItem> items;
+
+        public IEnumerable<IOrderItem> Items => throw new NotImplementedException(); //{ get; }
+
+        public double Subtotal => 0; //{ get; }
+
+        public uint OrderNumber { get; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void Add(IOrderItem item)
+        {
+
+        }
+
+        public void Remove(IOrderItem item)
+        {
+
+        }
+    }
+}
